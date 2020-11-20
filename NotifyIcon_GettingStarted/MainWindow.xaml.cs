b1ab50d9ffbc4d01521f3146bdb7786972faf361
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-11-20 18:37:41 +0530

    Added sample for creating the NotifyIcon

---
+++ NotifyIcon_GettingStarted/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+
+namespace NotifyIcon_GettingStarted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            notify.ShowBalloonTip(3000);
+        }
+    }
+}
